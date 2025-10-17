b48f188d3ef89bd700b50335117d29340dc8927f
Marek Polewski <marek.polewski.stud@pw.edu.pl>
2025-10-17 10:29:32 +0200

    lab1 Marek Polewski

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,29 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+    private:
+        double x,y;
+    public:
+        Wektor2D(){
+            x=0; y=0;
+        };
+        Wektor2D(double x1, double x2){
+            x=x1; y=x2;
+        };
+
+        void setX(double new_x){x = new_x;};
+        void setY(double new_y){y = new_y;};
+
+        double getX(){return x;};
+        double getY(){return y;};
+        
 };
+
+Wektor2D operator+ (Wektor2D v1, Wektor2D v2){
+    return Wektor2D(
+        v1.getX() + v2.getX(), v1.getY() + v2.getY()
+    );
+}
+
+double operator* (Wektor2D v1, Wektor2D v2){
+    return v1.getX() * v2.getX() + v1.getY() * v2.getY();
+}
